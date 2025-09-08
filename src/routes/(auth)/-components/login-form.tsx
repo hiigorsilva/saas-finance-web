b3e81c3c93ced1c0fba33f52962b24f999dd3b24
Higor Code <higorscontato@gmail.com>
2025-09-08 11:58:01 -0300

    feat: add login page with login-form component

---
+++ src/routes/(auth)/-components/login-form.tsx
@@ -0,0 +1,139 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Link } from '@tanstack/react-router'
+import { EyeIcon, EyeOffIcon, LockIcon, MailIcon } from 'lucide-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { type LoginFormType, loginFormSchema } from '@/schemas/login-form'
+
+export function LoginForm() {
+  const [showPassword, setShowPassword] = useState(true)
+
+  const form = useForm<LoginFormType>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (data: LoginFormType) => {
+    console.log('REGISTER', data)
+  }
+
+  const handleToggleShowPassword = () => {
+    setShowPassword(prevState => !prevState)
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-6"
+      >
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem className="flex flex-col gap-0">
+              <div className="space-y-3">
+                <FormLabel className="font-semibold text-base text-foreground leading-none">
+                  E-mail
+                </FormLabel>
+                <FormControl>
+                  <div className="flex items-center border rounded-md px-3 has-[input:focus-within]:border-ring has-[input:focus-within]:ring-ring/50 has-[input:focus-within]:ring-[2px]">
+                    <MailIcon
+                      className="size-5 text-muted-foreground"
+                      strokeWidth={1}
+                    />
+                    <Input
+                      className="border-0 focus-visible:border-0 focus-visible:ring-0"
+                      placeholder="Digite seu email"
+                      {...field}
+                    />
+                  </div>
+                </FormControl>
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="relative flex flex-col gap-0">
+              <div className="space-y-3">
+                <FormLabel className="font-semibold text-base text-foreground leading-none">
+                  Senha
+                </FormLabel>
+                <FormControl>
+                  <div className="flex items-center border rounded-md pl-3 has-[input:focus-within]:border-ring has-[input:focus-within]:ring-ring/50 has-[input:focus-within]:ring-[2px]">
+                    <LockIcon
+                      className="size-5 text-muted-foreground"
+                      strokeWidth={1}
+                    />
+                    <Input
+                      className="border-0 focus-visible:border-0 focus-visible:ring-0"
+                      placeholder="Digite sua senha"
+                      type={showPassword ? 'password' : 'text'}
+                      {...field}
+                    />
+                    <Button
+                      onClick={handleToggleShowPassword}
+                      className="border-l border-border rounded-none"
+                      variant="ghost"
+                      size="icon"
+                      type="button"
+                    >
+                      {showPassword && (
+                        <EyeIcon
+                          className="size-4 text-muted-foreground"
+                          strokeWidth={1}
+                        />
+                      )}
+
+                      {!showPassword && (
+                        <EyeOffIcon
+                          className="size-4 text-muted-foreground"
+                          strokeWidth={1}
+                        />
+                      )}
+                    </Button>
+                  </div>
+                </FormControl>
+              </div>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex justify-end items-center">
+          <Link
+            to="/"
+            className="text-base text-foreground underline transition hover:text-muted-foreground"
+          >
+            Esqueci minha senha
+          </Link>
+        </div>
+
+        <Button
+          className="drop-shadow-lg drop-shadow-muted-foreground/25 transition"
+          variant="gradient"
+          type="submit"
+        >
+          Entrar
+        </Button>
+      </form>
+    </Form>
+  )
+}
--- src/routes/(auth)/login/route.tsx
+++ src/routes/(auth)/login/route.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Container } from '@/components/layout/container'
+import { LoginForm } from '../-components/login-form'
 
 export const Route = createFileRoute('/(auth)/login')({
   component: LoginPage,
@@ -7,10 +7,27 @@ export const Route = createFileRoute('/(auth)/login')({
 
 function LoginPage() {
   return (
-    <div>
-      <Container>
-        <h1>Página de Login</h1>
-      </Container>
-    </div>
+    <section className="flex-auto grid grid-cols-1 md:grid-cols-2">
+      {/* FORM REGISTER */}
+      <div className="flex flex-col justify-center items-center p-5 lg:p-0">
+        <div className="max-w-[28rem] w-full flex flex-col gap-8">
+          <div className="flex flex-col items-center sm:items-start gap-3">
+            <h1 className="font-semibold text-2xl sm:text-3xl text-center sm:text-start text-foreground leading-none tracking-tight">
+              Entre na sua conta
+            </h1>
+            <p className="text-base text-center sm:text-start text-muted-foreground text-pretty">
+              Bem vindo(a) de volta a sua dashboard financeira.
+            </p>
+          </div>
+
+          <LoginForm />
+        </div>
+      </div>
+
+      {/* IMAGE COVER */}
+      <div className="hidden md:flex relative overflow-hidden">
+        <div className="absolute inset-0 bg-gradient-to-r from-yellow-200 to-yellow-400 to-120%"></div>
+      </div>
+    </section>
   )
 }
