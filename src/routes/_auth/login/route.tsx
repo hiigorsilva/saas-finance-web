cc085783b1dd148117e531036f4da176b4bf204e
Higor Code <higorscontato@gmail.com>
2025-09-09 12:05:20 -0300

    refactor: rename auth folder to tanstack router best practices

--- src/routes/_auth/login/route.tsx
+++ src/routes/_auth/login/route.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { LoginForm } from '../-components/login-form'
 
-export const Route = createFileRoute('/(auth)/login')({
+export const Route = createFileRoute('/_auth/login')({
   component: LoginPage,
 })
 
