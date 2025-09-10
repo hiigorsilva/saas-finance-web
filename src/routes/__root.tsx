9d7ebbced599847906b6707864968a9d1757a6cf
Higor Code <higorscontato@gmail.com>
2025-09-10 15:36:58 -0300

    feat: create a page not found and set dynamic titles on pages

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,26 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRoute, HeadContent, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { NotFoundPage } from '@/components/layout/not-found'
 import { Screen } from '@/components/layout/screen'
 
-export const Route = createRootRoute({ component: RootLayout })
+export const Route = createRootRoute({
+  component: RootLayout,
+  head: () => ({
+    meta: [
+      {
+        title: 'Luna | Sua assistente financeira',
+        content:
+          'Registre todas as suas receitas e desepesas e tome o controle da sua vida financeira.',
+      },
+    ],
+  }),
+  notFoundComponent: NotFoundPage,
+})
 
 function RootLayout() {
   return (
     <>
+      <HeadContent />
       <Screen>
         <Outlet />
       </Screen>
