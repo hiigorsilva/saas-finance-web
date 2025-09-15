ae03671e5d7cac1b324377e16388f5d10d2706e4
Higor Code <higorscontato@gmail.com>
2025-09-15 00:05:08 -0300

    feat: add details-item-member-form component

--- src/schemas/invite-member-form.ts
+++ src/schemas/invite-member-form.ts
@@ -1,6 +1,6 @@
 import z from 'zod'
 
 export const inviteMemberFormSchema = z.object({
-  email: z.email(),
+  email: z.email('Informe um e-mail válido'),
 })
 export type InviteMemberFormSchemaType = z.infer<typeof inviteMemberFormSchema>
