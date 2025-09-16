c97f01791037c702efafa43d9c450e3840b5dee6
Higor Code <higorscontato@gmail.com>
2025-09-16 17:46:20 -0300

    feat: update dashboard page

--- src/routes/(private)/app/$workspaceId/index.tsx
+++ src/routes/(private)/app/$workspaceId/index.tsx
@@ -9,6 +9,8 @@ import { DashBoardCardChart } from './-components/dashboard-card-chat'
 import { DashBoardCardExpense } from './-components/dashboard-card-expense'
 import { DashBoardCardIncome } from './-components/dashboard-card-income'
 import { DashBoardCardInvestiment } from './-components/dashboard-card-investiment'
+import { DashBoardCardLastTransactions } from './-components/dashboard-card-last-transactions'
+import { DashboardCardLatePayment } from './-components/dashboard-card-late-payment'
 import { DashboardTimeSelect } from './-components/dashboard-time-select'
 
 export const Route = createFileRoute('/(private)/app/$workspaceId/')({
@@ -51,7 +53,7 @@ function DashboardPage() {
         </div>
       </div>
 
-      <div className="w-full flex-auto grid grid-cols-3 gap-6">
+      <div className="w-full flex-auto grid grid-cols-3 gap-3">
         {/* LEFTSIDE */}
         <div className="flex flex-col gap-6 col-span-2 col-start-1">
           {/* CARDS */}
@@ -66,10 +68,13 @@ function DashboardPage() {
           </div>
 
           <DashBoardCardChart />
+          <DashBoardCardLastTransactions />
         </div>
 
         {/* RIGHTSIDE */}
-        <div className="col-span-1 col-start-3 p-6">RIGHT</div>
+        <div className="col-span-1 col-start-3">
+          <DashboardCardLatePayment />
+        </div>
       </div>
     </Container>
   )
