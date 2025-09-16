cf858d0e62ab185a09742af31dde609d7783e3f2
Higor Code <higorscontato@gmail.com>
2025-09-16 18:15:44 -0300

    style: change size gap of the late payment component to dashboard page

--- src/routes/(private)/app/$workspaceId/-components/dashboard-card-late-payment.tsx
+++ src/routes/(private)/app/$workspaceId/-components/dashboard-card-late-payment.tsx
@@ -43,12 +43,12 @@ export function DashboardCardLatePayment() {
               </TableCell>
               <TableCell>
                 <div className="flex flex-col items-end gap-1">
-                  <div className="w-fit h-fit flex justify-center items-center gap-1 text-sm text-red-500 bg-red-500/10 border border-red-500/25 rounded-full pl-1.5 pr-2 py-0.5">
+                  <div className="w-fit h-fit flex justify-center items-center gap-1 text-sm text-red-500 leading-none bg-red-500/10 border border-red-500/25 rounded-full pl-1.5 pr-2 py-1">
                     <ClockIcon className="size-3 shrink-0 text-red-500" />
                     Atrasada
                   </div>
-                  <span className="inline-flex font-normal text-xs text-muted-foreground leading-none px-1">
-                    Ontem
+                  <span className="inline-flex font-normal text-xs text-muted-foreground capitalize leading-none px-1">
+                    Amanhã
                   </span>
                 </div>
               </TableCell>
