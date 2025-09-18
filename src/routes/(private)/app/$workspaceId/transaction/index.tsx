f5f84f53049832140c91cd821f842dd5b7cc8c52
Higor Code <higorscontato@gmail.com>
2025-09-18 18:07:18 -0300

    feat: update transaction page

--- src/routes/(private)/app/$workspaceId/transaction/index.tsx
+++ src/routes/(private)/app/$workspaceId/transaction/index.tsx
@@ -1,6 +1,12 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
+import { ChevronLeftIcon, PlusIcon } from 'lucide-react'
 import { Container } from '@/components/layout/container'
+import { TitleIconPage } from '@/components/layout/title-icon-page'
+import { TitlePage } from '@/components/layout/title-page'
 import { Button } from '@/components/ui/button'
+import { transactionResponse } from '@/data/requests/transactions'
+import { TransactionSearchFilterForm } from './-components/search-filter-form'
+import { TransactionTable } from './-components/transaction-table'
 
 export const Route = createFileRoute(
   '/(private)/app/$workspaceId/transaction/'
@@ -17,16 +23,41 @@ export const Route = createFileRoute(
 
 function TransactionPage() {
   const { workspaceId } = Route.useParams()
+  const router = Route.useNavigate()
+
+  const { data: transactions } = transactionResponse.body
+
+  const handleNavigateBack = () => {
+    router({
+      to: '/app/$workspaceId',
+      params: { workspaceId: workspaceId },
+    })
+  }
 
   return (
-    <Container className="space-y-6">
-      <h1>Página de Transações</h1>
+    <Container className="gap-6 py-6">
+      {/* TITLE */}
+      <div className="flex items-center gap-2">
+        <TitleIconPage handleNavigateBack={handleNavigateBack}>
+          <ChevronLeftIcon />
+        </TitleIconPage>
+        <TitlePage>Transações</TitlePage>
+      </div>
 
-      <Button className="w-fit" variant="gradient" asChild>
-        <Link to=".." params={{ workspaceId: workspaceId }}>
-          Voltar para Dashboard
-        </Link>
-      </Button>
+      {/* FILTER */}
+      <div className="flex justify-between items-center gap-6">
+        <div className="max-w-2xs w-full">
+          <TransactionSearchFilterForm />
+        </div>
+
+        <Button variant="gradient">
+          <PlusIcon className="size-5 shrink-0 text-foreground" />
+          <span>Adicionar Transação</span>
+        </Button>
+      </div>
+
+      {/* TRANSACTION TABLE */}
+      <TransactionTable transactions={transactions} />
     </Container>
   )
 }
