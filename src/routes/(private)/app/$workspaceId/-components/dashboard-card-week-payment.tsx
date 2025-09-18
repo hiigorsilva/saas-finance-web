e8529a331472838ce146a2289961a07b5c7af337
Higor Code <higorscontato@gmail.com>
2025-09-17 23:12:29 -0300

    feat: create week payments component with two variants (empty and filled)

--- src/routes/(private)/app/$workspaceId/-components/dashboard-card-week-payment.tsx
+++ src/routes/(private)/app/$workspaceId/-components/dashboard-card-week-payment.tsx
@@ -1,16 +1,23 @@
-import { ClockIcon } from 'lucide-react'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import type { TransactionType } from '@/data/requests/transactions'
+import { DashboardCardWeekPaymentEmpty } from './dashboard-card-week-payment-empty'
+import { DashboardCardWeekPaymentRow } from './dashboard-card-week-payment-row'
 
-export function DashboardCardWeekPayment() {
+type DashboardCardWeekPaymentProps = {
+  weekPayments: TransactionType[]
+}
+
+export function DashboardCardWeekPayment({
+  weekPayments,
+}: DashboardCardWeekPaymentProps) {
   return (
     <Card>
       <CardHeader className="gap-6">
@@ -28,31 +35,15 @@ export function DashboardCardWeekPayment() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
-              <TableCell className="font-semibold">
-                <div className="w-full flex justify-between items-center gap-3">
-                  <div className="flex flex-col">
-                    <h3 className="font-semibold text-base text-foreground">
-                      Aluguel do apartamento
-                    </h3>
-                    <span className="font-normal text-sm text-muted-foreground">
-                      04 Dez de 2024
-                    </span>
-                  </div>
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className="flex flex-col items-end gap-1">
-                  <div className="w-fit h-fit flex justify-center items-center gap-1 text-sm text-blue-500 leading-none bg-blue-500/10 border border-blue-500/25 rounded-full pl-1.5 pr-2 py-1">
-                    <ClockIcon className="size-3 shrink-0 text-blue-500" />
-                    Atrasada
-                  </div>
-                  <span className="inline-flex font-normal text-xs text-muted-foreground capitalize leading-none px-1">
-                    Ontem
-                  </span>
-                </div>
-              </TableCell>
-            </TableRow>
+            {weekPayments.length > 0 && (
+              <DashboardCardWeekPaymentRow weekPayments={weekPayments} />
+            )}
+
+            {weekPayments.length === 0 && (
+              <DashboardCardWeekPaymentEmpty>
+                Nenhum pagamento para esta semana
+              </DashboardCardWeekPaymentEmpty>
+            )}
           </TableBody>
         </Table>
       </CardContent>
