6bd5b36237e0cf1d531f9757394d1d595cbe11d7
Higor Code <higorscontato@gmail.com>
2026-03-07 19:16:18 -0300

    feat: add workspace service and implement workspaces fetching logic

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,3 +2,11 @@ export interface IHttpResponse<T> {
   statusCode: number
   body: T
 }
+
+export interface IPaginateResponse<T> {
+  data: T[]
+  totalCount: number
+  totalPages: number
+  currentPage: number
+  limit: number
+}
