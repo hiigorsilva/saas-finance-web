b9fb454883c7c11fb1e15cb82cd2fa434b916c63
Higor Code <higorscontato@gmail.com>
2026-04-27 20:25:57 -0300

    feat: update dashboard week payment row to display dynamic payment details

--- src/routes/(private)/app/$workspaceId/-components/dashboard-card-week-payment-row.tsx
+++ src/routes/(private)/app/$workspaceId/-components/dashboard-card-week-payment-row.tsx
@@ -1,6 +1,11 @@
 import { ClockIcon } from 'lucide-react'
 import { TableCell, TableRow } from '@/components/ui/table'
 import type { TransactionType } from '@/data/requests/transactions'
+import {
+  dateFormat,
+  dateFormatDistanceToNow,
+  dateIsBeforeToday,
+} from '@/utils/date-format'
 
 type DashboardCardWeekPaymentRowProps = {
   weekPayments: TransactionType[]
@@ -9,33 +14,37 @@ type DashboardCardWeekPaymentRowProps = {
 export function DashboardCardWeekPaymentRow({
   weekPayments,
 }: DashboardCardWeekPaymentRowProps) {
-  console.log('Week Payments:', weekPayments)
-
   return (
-    <TableRow>
-      <TableCell className="font-semibold">
-        <div className="w-full flex justify-between items-center gap-3">
-          <div className="flex flex-col">
-            <h3 className="font-semibold text-base text-foreground">
-              Aluguel do apartamento
-            </h3>
-            <span className="font-normal text-sm text-muted-foreground">
-              04 Dez de 2024
-            </span>
-          </div>
-        </div>
-      </TableCell>
-      <TableCell>
-        <div className="flex flex-col items-end gap-1">
-          <div className="w-fit h-fit flex justify-center items-center gap-1 text-sm text-blue-500 leading-none bg-blue-500/10 border border-blue-500/25 rounded-full pl-1.5 pr-2 py-1">
-            <ClockIcon className="size-3 shrink-0 text-blue-500" />
-            Atrasada
-          </div>
-          <span className="inline-flex font-normal text-xs text-muted-foreground capitalize leading-none px-1">
-            Ontem
-          </span>
-        </div>
-      </TableCell>
-    </TableRow>
+    <>
+      {weekPayments.map(payment => (
+        <TableRow key={payment.id}>
+          <TableCell className="font-semibold">
+            <div className="w-full flex justify-between items-center gap-3">
+              <div className="flex flex-col">
+                <h3 className="font-semibold text-base text-foreground">
+                  {payment.name}
+                </h3>
+                <span className="font-normal text-sm text-muted-foreground">
+                  {dateFormat(payment.paymentDate)}
+                </span>
+              </div>
+            </div>
+          </TableCell>
+          <TableCell>
+            <div className="flex flex-col items-end gap-1">
+              <div className="w-fit h-fit flex justify-center items-center gap-1 text-sm text-blue-500 leading-none bg-blue-500/10 border border-blue-500/25 rounded-full pl-1.5 pr-2 py-1">
+                <ClockIcon className="size-3 shrink-0 text-blue-500" />
+                {dateIsBeforeToday(payment.paymentDate)
+                  ? 'Atrasada'
+                  : 'Pendente'}
+              </div>
+              <span className="inline-flex font-normal text-xs text-muted-foreground capitalize leading-none px-1">
+                {dateFormatDistanceToNow(payment.paymentDate)}
+              </span>
+            </div>
+          </TableCell>
+        </TableRow>
+      ))}
+    </>
   )
 }
--- src/utils/date-format.ts
+++ src/utils/date-format.ts
@@ -1,4 +1,4 @@
-import { format } from 'date-fns'
+import { format, formatDistanceToNow, isBefore } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 
 export function dateFormat(date: Date | string) {
@@ -15,4 +15,21 @@ export function dateFormatLong(date: Date | string) {
   return format(parseDate, 'PPP', {
     locale: ptBR,
   })
+}
+
+export function dateFormatDistanceToNow(date: Date | string) {
+  const parseDate = new Date(date)
+
+  const periodValue = formatDistanceToNow(parseDate, {
+    addSuffix: true,
+    locale: ptBR,
+  })
+  return periodValue
+}
+
+export function dateIsBeforeToday(date: Date | string) {
+  const parseDate = new Date(date)
+  const today = new Date()
+
+  return isBefore(parseDate, today)
 }
