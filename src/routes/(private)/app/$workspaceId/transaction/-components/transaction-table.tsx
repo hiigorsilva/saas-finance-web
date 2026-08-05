016e1b023b96cb2d95426e31214b996a6ba858a7
Higor Silva Soares <higorscontato@gmail.com>
2026-08-05 11:29:53 -0300

    feat: add empty transactions component

--- src/routes/(private)/app/$workspaceId/transaction/-components/transaction-table.tsx
+++ src/routes/(private)/app/$workspaceId/transaction/-components/transaction-table.tsx
@@ -1,3 +1,11 @@
+import { StickyNoteIcon } from 'lucide-react'
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from '@/components/ui/empty'
 import {
   Table,
   TableBody,
@@ -14,7 +22,7 @@ type TransactionTableProps = {
 
 export function TransactionTable({ transactions }: TransactionTableProps) {
   return (
-    <div className="flex flex-col border border-primary/10 rounded-lg overflow-hidden">
+    <div className="flex flex-col flex-1 border border-primary/10 rounded-lg overflow-hidden">
       <Table>
         <TableHeader>
           <TableRow className="bg-primary/10">
@@ -30,16 +38,35 @@ export function TransactionTable({ transactions }: TransactionTableProps) {
           </TableRow>
         </TableHeader>
 
-        <TableBody>
-          {transactions.length > 0 &&
-            transactions.map(transaction => (
+        {transactions.length > 0 && (
+          <TableBody>
+            {transactions.map(transaction => (
               <TransactionTableRow
                 key={transaction.id}
                 transaction={transaction}
               />
             ))}
-        </TableBody>
+          </TableBody>
+        )}
       </Table>
+      {transactions.length === 0 && (
+        <div className="flex flex-col flex-1 justify-center items-center gap-3">
+          <p className="text-sm leading-none text-center text-muted-foreground">
+            <Empty>
+              <EmptyHeader className="gap-0">
+                <EmptyMedia variant="icon">
+                  <StickyNoteIcon className="size-5 shrink-0 text-muted-foreground" />
+                </EmptyMedia>
+                <EmptyTitle>Nenhuma transação encontrada.</EmptyTitle>
+              </EmptyHeader>
+              <EmptyDescription>
+                Clique no botão "Nova Transação" para registrar suas despesas e
+                receitas.
+              </EmptyDescription>
+            </Empty>
+          </p>
+        </div>
+      )}
     </div>
   )
 }
